e577d2df62cde44e11c9a692644d4b065f378a8f
NGUYỄN PHÚC THỊNH <119058000+hieu79115@users.noreply.github.com>
2025-06-08 21:54:25 +0700

    git commit -m "Fix: remove output export and assetPrefix for Vercel"

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  assetPrefix: isProd ? "/hieu79115/" : "",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
